bfc15dfe4fa283e802444d33a72c41acc402d19d
xiaofeng2824 <xiaofeng2824@163.com>
2019-07-01 00:26:35 +0800

    MySqrt

---
+++ src/MyMath/MySqrt.java
@@ -0,0 +1,21 @@
+package MyMath;
+
+/**
+ * 求平方根
+ */
+public class MySqrt {
+    public int mySqrt(int x)
+    {
+        long n = x;
+        while(n*n > x)
+        {
+            n = (n + x/n) >> 1;
+        }
+        return (int)n;
+    }
+
+    public static void main(String[] args) {
+        MySqrt mySqrt = new MySqrt();
+        System.out.println(mySqrt.mySqrt(10));
+    }
+}
