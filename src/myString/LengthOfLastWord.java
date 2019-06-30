c658ba665373c39bcdbff8ab6d3fb0e66dfdf81e
xiaofeng2824 <xiaofeng2824@163.com>
2019-06-30 22:11:53 +0800

    字符串最后一个单词的长度

---
+++ src/myString/LengthOfLastWord.java
@@ -0,0 +1,29 @@
+package myString;
+
+/**
+ * 计算最后一个单词的长度
+ */
+public class LengthOfLastWord {
+
+    public int lengthOfLastWord(String s)
+    {
+        //字符串长度
+        int p = s.length()-1;
+
+        //最后一个单词的最后一个字母
+        while(p >=0 && s.charAt(p) == ' ') p--;
+        int end = p;
+        //最后一个单词的第一个字母
+        while(p >=0 && s.charAt(p) != ' ') p--;
+        return end - p;
+
+    }
+
+    public static void main(String[] args) {
+        LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
+        System.out.println(lengthOfLastWord.lengthOfLastWord("Hello world"));
+        System.out.println(lengthOfLastWord.lengthOfLastWord("I love you "));
+
+    }
+
+}
